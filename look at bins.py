eae6179754ce8ef5fdc492cf18b52a5fb8c4c587
parrajeremy <jeremy.parra@intel.com>
2018-01-24 09:44:08 -0500

    Rearrange things

--- look at bins.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-pickle_in = open("bin/bins.pickle","rb")
-binfilter = pickle.load(pickle_in)
-pickle_in = open("bin/bins_mod.pickle","rb")
-binfilter_wmod = pickle.load(pickle_in)
-
-print(binfilter_wmod['AOA01_mean'])
